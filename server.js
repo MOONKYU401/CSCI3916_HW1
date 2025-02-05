5d5a5b979690c70ac2df4f8d6da0197c92db2cea
MOONKYU401 <mmk6553@gmail.com>
2025-02-04 20:17:52 -0700

    update

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ app.use(express.json());
 app.post('/', (req, res) => {
     const acceptHeader = req.get('accept');
 
+    if (acceptHeader){
+      res.setHeader('Accept',acceptHeader)
+    }
     const responseBody = {
-        acceptHeader: acceptHeader,
+
         ...req.body
     };
 
